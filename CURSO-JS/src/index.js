a636fff449110f1cd594c0641879a8b7f08713d5
salvador437 <salvadorlopezsegura@gmail.com>
2024-08-22 13:51:46 +0200

    Add files via upload

---
+++ CURSO-JS/src/index.js
@@ -0,0 +1,86 @@
+
+// asignando en constantes los  elementos HTML implicados.
+const inputdni = document.querySelector(".input-dniNumero");
+const inputConversor = document.querySelector(".input-conversor");
+const btnCalcular = document.querySelector(".btn-calcular");
+const btnCalTemp = document.querySelector(".btn-cal-temp");
+
+// escuchando cuando se hace click en los botones.
+btnCalcular.addEventListener("click", calcularLetraDNI);
+btnCalTemp.addEventListener("click", calcularTemperaturaCF);
+
+// escuchando cuando tiene el foco el input dni.
+inputdni.addEventListener("focus", () => {
+  inputdni.value = "";
+  inputConversor.value = "";
+  document.querySelector("#resultado").textContent = "";
+});
+
+// escuchando cuando  tiene el  input temperatura.
+inputConversor.addEventListener("focus", () => {
+  inputConversor.value = "";
+  inputdni.value = "";
+  document.querySelector("#resultado-temperatura").textContent = "";
+});
+
+// escuchando cuando el input pierde el foco
+inputConversor.addEventListener("blur", () => {
+  btnCalTemp.style.display = "inline-block";
+});
+
+// commutación menú hanburguesa y navegador.
+function toggleMenu() {
+  const menu = document.getElementById("navbarMenu");
+  if (menu.className === "menu") {
+    menu.className += " responsive";
+  } else {
+    menu.className = "menu";
+  }
+}
+
+// cálculo de la letra del DNI.
+function calcularLetraDNI() {
+  // Verificar que el DNI sea un número de 8 dígitos
+  const dniNumero = document.querySelector(".input-dniNumero").value;
+  inputdni.addEventListener("input", function (event) {
+    if (/^[0-9]$/.test(event.key)) {
+      event.preventDefault();
+    }
+  });
+
+  if (dniNumero.length !== 8 || isNaN(dniNumero)) {
+    document.querySelector("#resultado").textContent =
+      "Por favor, introduzca sólo números de 8 dígitos.";
+    return;
+  }
+
+  // Seleccionar la letra mediate el algorritmo aplicado.
+  const letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+  const indice = dniNumero % 23;
+  const letraDNI = letras[indice];
+
+  document.querySelector("#resultado").textContent = ` El DNI 
+  completo es: ${dniNumero}${letraDNI} `;
+}
+
+//Cálculo de la conversión de grados C => F.
+function calcularTemperaturaCF() {
+  const gradosConversion = document.querySelector(".input-conversor").value;
+  if (gradosConversion.length > 3 || isNaN(gradosConversion) || gradosConversion.length < 1)
+  {
+    document.querySelector("#resultado-temperatura").textContent =
+    "Por favor, introduce  valores númericos de 3 cifras";
+    return;
+  }
+
+  let gradosFarhengeis = (9 / 5) * gradosConversion + 32;
+  gradosFarhengeis = gradosFarhengeis.toLocaleString("es-ES", {
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+  });
+
+  document.querySelector(
+    "#resultado-temperatura").textContent = `${gradosConversion} grados C son ${gradosFarhengeis} F `;
+}
+
+
---
+++ CURSO-JS/src/pagina2.js
@@ -0,0 +1,46 @@
+// asignando en constantes los  elementos HTML.
+const inputImcPeso = document.querySelector(".input-imcPeso");
+const inputImcAltura = document.querySelector(".input-imcAltura");
+const btnCalImc = document.querySelector(".btn-cal-imc");
+const resultadoIMC = document.querySelector("#resultado-imc");
+
+// escuchando cuando se hace click en el botón "btnCalImc".
+btnCalImc.addEventListener("click", calcularIMC);
+
+// escuchando cuando tiene el foco el "inputImcPeso".
+inputImcPeso.addEventListener("focus", () => {
+  inputImcPeso.value = "";
+  document.querySelector("#resultado-imc").textContent = "";
+});
+
+// escuchando cuando tiene el foco el inputImcAltura.
+inputImcAltura.addEventListener("focus", () => {
+  inputImcAltura.value = "";
+  document.querySelector("#resultado-imc").textContent = "";
+});
+
+// commutación menú hanburguesa y navegador.
+function toggleMenu() {
+  const menu = document.getElementById("navbarMenu");
+  if (menu.className === "menu") {
+    menu.className += " responsive";
+  } else {
+    menu.className = "menu";
+  }
+}
+
+// calcula el indice de masa corporal IMC
+function calcularIMC() {
+  // valores de los inputs peso y altura.
+  const valorPeso = document.querySelector(".input-imcPeso").value;
+  let valorAltura = document.querySelector(".input-imcAltura").value;
+  valorAltura = valorAltura / 100;
+  let valorResultado = valorPeso / valorAltura ** 2;
+  // cambiando el punto por la coma en los decimales del resultado
+  valorResultado = valorResultado.toLocaleString("es-ES", {
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+  });
+
+  resultadoIMC.textContent = "El  IMC será de: " + valorResultado;
+}
