abfb674243425f12c8f912e5105ed5ffdbda3d47
salvador <salva@gmail,com>
2024-12-06 10:19:03 +0100

    modified:   hojas-estilo/calculoIMC.css 	modified:   hojas-estilo/index.css 	modified:   src/notificacionError.js 	modified:   src/notificacionErrorIMC.js

--- src/notificacionError.js
+++ src/notificacionError.js
@@ -1,10 +1,10 @@
 // Obtener elementos del DOM
 const modal = document.getElementById("myModal");
-// const openModalBtn = document.getElementById("openModalBtn");
+
 const closeBtn = document.getElementsByClassName("close")[0];
 
 
-// Cuando el usuario hace clic en la 'x', se cierra el modal
+// Cuando el usuario hace clic en el historial atras de navegación, se se vuelve a cargar la pagina para no salir de ella
 closeBtn.onclick = function () {
   modal.style.display = "none";
   open("./index.html", target="_self"   );
--- src/notificacionErrorIMC.js
+++ src/notificacionErrorIMC.js
@@ -1,8 +1,9 @@
 // Obtener elementos del DOM
 const modal = document.getElementById("myModal");
-// const openModalBtn = document.getElementById("openModalBtn");
+
 const closeBtn = document.getElementsByClassName("close2")[0];
 
+// Cuando el usuario hace clic en el historial atras de navegación, se se vuelve a cargar la pagina para no salir de ella
 closeBtn.onclick = function () {
   modal.style.display = "none";
   open("./calculoIMC.html", target="_self");
