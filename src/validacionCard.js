a2a901c117fe3fe9b352deb9b3d3d63dfdcea9f4
salvador <salva@gmail,com>
2024-12-07 12:43:17 +0100

    modified:   calculoIMC.html 	modified:   hojas-estilo/calculoIMC.css 	modified:   index.html 	modified:   link.html 	modified:   src/validacionCard.js 	modified:   template.html

--- src/validacionCard.js
+++ src/validacionCard.js
@@ -1,24 +1,23 @@
 // asignando en constantes los  elementos HTML.
 const inputValidacion = document.querySelector(".input-validacion");
-
 const btnCalValidacion = document.querySelector(".btn-cal-validacion");
 const resultadoValidacion = document.querySelector("#resultado-validacion");
 
 // escuchando cuando se hace click  en el botón calcular
 btnCalValidacion.addEventListener("click", calcularValidacion);
 
-// escuchando cuando tiene el foco el input validacion
+// escuchando cuando tiene el foco el input validación
 inputValidacion.addEventListener("focus", () => {
   inputValidacion.value = "";
   document.querySelector("#resultado-validacion").textContent = "";
 });
 
-// cálculo de la validacion del numero de la card
+// cálculo de la validación del número de la card
 function calcularValidacion(numero){
   
 }
 
-
+// commutación menú hanburguesa y navegador.
 function toggleMenu() {
   const menu = document.getElementById("navbarMenu");
   if (menu.className === "menu") {
