e72138e94427a37a153c744ea0ce2dd59448e7a0
salvador <salva@gmail,com>
2024-12-14 16:40:14 +0100

    modified:   formulario.html 	modified:   hojas-estilo/formulario.css 	modified:   hojas-estilo/index.css 	modified:   src/formulario.js

--- src/formulario.js
+++ src/formulario.js
@@ -1,20 +1,67 @@
 const closeBtn = document.getElementsByClassName("close4")[0];
-const inputNombre = document.querySelector("#nombre"); 
-const inputMail = document.querySelector("#mail")
+const inputNombre = document.querySelector("#nombre");
 const inputApellidos = document.querySelector("#apellidos");
+const inputMail = document.querySelector("#mail");
 const inputTelefono = document.querySelector("#telefono");
+const inputCiudad = document.querySelector("#ciudad");
+const inputPais = document.querySelector("#pais");
 const inputMensaje = document.querySelector("#msg");
 
+inputNombre.addEventListener("keydown", function (event) {
+if(event.key ==="Escape"){
+    inputNombre.value = "";
+  }
+});
 
+inputApellidos.addEventListener("keydown", function (event) {
+  if(event.key ==="Escape"){
+      inputApellidos.value = "";
+    }
+  });
+
+inputMail.addEventListener("keydown", function (event) {
+  if(event.key ==="Escape"){
+    inputMail.value = "";
+  }
+});
+
+inputTelefono.addEventListener("keydown", function (event) {
+  if(event.key ==="Escape"){
+      inputTelefono.value = "";
+    }
+  });
+
+  inputCiudad.addEventListener("keydown", function (event) {
+    if(event.key ==="Escape"){
+        inputCiudad.value = "";
+      }
+    });
+
+    inputPais.addEventListener("keydown", function (event) {
+      if(event.key ==="Escape"){
+          inputPais.value = "";
+        }
+      });
+
+      inputMensaje.addEventListener("keydown", function (event) {
+        if(event.key ==="Escape"){
+            inputMensaje.value = "";
+          }
+        });
 
 document.addEventListener("keydown", function (event) {
   if (event.ctrlKey && event.key === "z") {
     window.open("./index.html", (target = "_self"));
   }
-  if (event.key === "Escape") {
-    inputMail.value=""
+  if (event.altKey &&  event.key === "b") {
+    inputNombre.value = "";
+    inputApellidos.value = "";
+    inputMail.value = "";
+    inputTelefono.value = "";
+    inputCiudad.value = "";
+    inputPais.value = "";
+    inputMensaje.value = "";
   }
-  
 });
 
 closeBtn.onclick = function () {
