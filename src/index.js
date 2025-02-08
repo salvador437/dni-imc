ddd8398c6c6107fb1882f26143259fef1be64e84
salvador <salvadorlopezsegura@gmail.com>
2025-02-08 21:09:04 +0100

    modified:   hojas-estilo/index.css 	modified:   index.html 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import  { pantallaCompleta, toggleMenu2, subirTodo }  from "./funciones.js";
 
 const menu2 = document.querySelector(".menu-icon");
 const subir = document.querySelector(".subir");
+const flechaSubir = document.querySelector(".flecha");
 
 menu2.addEventListener("click",toggleMenu2);
 subir.addEventListener("click", subirTodo);
+flechaSubir.addEventListener("click", subirTodo); 
 
 // escuchando cuando se pulsa una tecla
 document.addEventListener("keydown", function (event) {
