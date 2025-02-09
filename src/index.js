5d8b04c6621205cb0765e4da0cd80ddaa274b025
salvador <salvadorlopezsegura@gmail.com>
2025-02-09 09:37:26 +0100

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -11,16 +11,12 @@ flechaSubir.classList.remove("flecha");
 menu2.addEventListener("click",toggleMenu2);
 subir.addEventListener("click", subirTodo);
 flechaSubir.addEventListener("click", subirTodo);
-
-
 window.addEventListener("scroll", handleScroll);
-// escuchando cuando se pulsa una tecla
+
 document.addEventListener("keydown", function (event) {
-  
   if ( (event.altKey &&  event.key === "p" ) ||
         (event.altKey &&  event.key === "P"
   ) ) {
     pantallaCompleta();
   }
-
 });
